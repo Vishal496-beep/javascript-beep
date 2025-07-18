b854857427099c776d8bb0f40896bff53c6a33bb
Vishal496-beep <vishal.k08032004@gmail.com>
2025-07-18 10:26:26 +0000

    02_conversionOperations

---
+++ 02_Operations.js
@@ -0,0 +1,48 @@
+// operations in js */
+
+let value = 5
+let negValue = -value
+
+// console.log(negValue)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)*/
+
+let str1 = "vishal"
+let str2 = " software"
+
+let str3 = str1+str2
+//console.log(str3);
+/*
+console.log("1" + 2 )
+console.log("2" + 1)
+console.log("1" + "2");
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")*/
+/*
+console.log(+true);
+// console.log(true+);
+console.log(+"");*/
+
+let gameCount = 500
+//++gamecount;
+// console.log(gameCount)
+/*
+console.log(2 > 2)
+console.log(2 >= 2)
+console.log(2 < 2)
+console.log(2 == 2)
+console.log(2 != 2)*/
+
+/*console.log("2" > 1);
+console.log("02" > 1);*/
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);

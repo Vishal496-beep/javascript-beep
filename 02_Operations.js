a755de35b788bfe5567248f97632f8f50ded7277
Vishal496-beep <vishal.k08032004@gmail.com>
2025-07-18 13:09:23 +0000

    operations in javascript

--- 02_Operations.js
+++ 02_Operations.js
@@ -46,3 +46,38 @@ console.log(null == 0);
 console.log(null >= 0);
 
 console.log(undefined == 0);
+
+ let score = "vishal"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber)
+
+// NaN not a number
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+//  false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 =>false
+// "" => false
+// "abc" => true
+
+let SomeNumber = 3568
+
+let stringIsName = String(SomeNumber)
+// console.log(typeof stringIsName)
+
+let grandOrder = 456
+
+let numberIsGrand = BigInt(grandOrder)
+console.log(numberIsGrand)
---
+++ 03_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //newer version of code
+
+//alert("hello")//we are using node.js , not browser
+
+//console.log( 3 =
+   // 3//code readablity should be high
+
+console.log("vishal")
+
+let name = "vishal"
+let age = 21
+let idLoggedIn = true
+let state;
+
+// primitive datatypes
+// number => 2 to the power of 53
+// bigint => used in larger numbers such as trading
+// string => double ""
+// boolean => true/false
+// null => is a object/ standalone value/ empty
+// undefined => same as state which we haven't defined
+// symbols => unique
+
+// non-premitive datatypes 
+/*array, function, objects */
+
+// obj
+console.log(typeof null ) // object
+console.log(typeof undefined ) //undefined
+

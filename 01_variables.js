01eef66e84d9a05bbda88761c61df9220b035634
Vishal496-beep <vishal.k08032004@gmail.com>
2025-07-18 10:21:54 +0000

    01variables

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountid = 25593
+let accountEmail = "vishal@gmail.com"
+var accountpassword = "1234568"
+accountcity = "jaipur"
+
+// accountid = 52655//not allowed
+
+accountEmail = "bhjgfheb@gnhje.com"
+accountpassword =  251265
+accountcity = "kashmir"
+
+console.log(accountid)
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([ accountid , accountEmail, accountpassword, accountcity])
